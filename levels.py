87e72e91ff32245972e721efbab3051b143aca41
Paul Kern <paul.kern.fr@gmail.com>
2024-08-19 10:42:00 +0200

    levels 5 and 6

--- levels.py
+++ levels.py
@@ -48,4 +48,27 @@ def get_level(number: int) -> LevelData:
             Cell(0, 4),
         ]
 
+    elif number == 4:
+        set_main_params(level_data, 16, 1, 192)
+        level_data.cells = [
+            Cell(0, 0, 16),
+            Cell(16, 0, 8), Cell(16, 8, 8),
+            Cell(24, 0, 4), Cell(24, 4, 4), Cell(24, 8, 4), Cell(24, 12, 4),
+            Cell(28, 0, 2), Cell(28, 2, 2), Cell(28, 4, 2), Cell(28, 6, 2),
+            Cell(28, 8, 2), Cell(28, 10, 2), Cell(28, 12, 2), Cell(28, 14, 2),
+            Cell(30, 0), Cell(30, 1), Cell(30, 2), Cell(30, 3),
+            Cell(30, 4), Cell(30, 5), Cell(30, 6), Cell(30, 7),
+            Cell(30, 8), Cell(30, 9), Cell(30, 10), Cell(30, 11),
+            Cell(30, 12), Cell(30, 13), Cell(30, 14), Cell(30, 15)
+        ]
+        pass
+
+    elif number == 5:
+        set_main_params(level_data, 64, 1, [60, 80])
+        level_data.cells = [
+            Cell(0, 0), Cell(1, 0), Cell(5, 0),
+            Cell(0, 1), Cell(1, 1), Cell(3, 1, _type=CellType.FORBIDDEN), Cell(5, 1, _type=CellType.MULT_2), Cell(6, 1),
+            Cell(0, 2), Cell(1, 2), Cell(5, 2),
+        ]
+
     return level_data
